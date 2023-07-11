4e02abba4d194c03ef02254d36c6649c39879572
Aaron Kaw <aaronjkaw@gmail.com>
2023-07-11 11:52:16 +1000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.36

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using OceanSonar
+using Documenter
+
+DocMeta.setdocmeta!(OceanSonar, :DocTestSetup, :(using OceanSonar); recursive=true)
+
+makedocs(;
+    modules=[OceanSonar],
+    authors="Aaron Kaw <aaronjkaw@gmail.com>",
+    repo="https://github.com/kapple19/OceanSonar.jl/blob/{commit}{path}#{line}",
+    sitename="OceanSonar.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://kapple19.github.io/OceanSonar.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/kapple19/OceanSonar.jl",
+    devbranch="main",
+)
