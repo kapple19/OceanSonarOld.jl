41bc37b45f85f91c86b050cc84ff1a6ff1be77d1
kapple19 <aaronjkaw@gmail.com>
2023-07-11 13:18:37 +1000

    Added overall structure to source code

--- src/OceanSonar.jl
+++ src/OceanSonar.jl
@@ -3,5 +3,8 @@ module OceanSonar
 export OcnSon
 
 include("preamble.jl")
+include("auxiliary/Auxiliary.jl")
+include("structures/Structures.jl")
+include("models/Models.jl")
 
 end
---
+++ src/auxiliary/Auxiliary.jl
@@ -0,0 +1,3 @@
+module Auxiliary
+
+end
